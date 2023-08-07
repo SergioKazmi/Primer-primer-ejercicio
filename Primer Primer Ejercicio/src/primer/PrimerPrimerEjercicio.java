b6951f22fe051e3400a186a1e5d460944e8b2b41
PabloASchamber <pablo.schamber@hotmail.com>
2023-08-07 17:16:15 -0300

    Update PrimerPrimerEjercicio.java
    hola

--- Primer Primer Ejercicio/src/primer/PrimerPrimerEjercicio.java
+++ Primer Primer Ejercicio/src/primer/PrimerPrimerEjercicio.java
@@ -4,13 +4,13 @@ package primer;
 
 /**
  *
- * @sergiokaz 
+ * lee esto
  */
 public class PrimerPrimerEjercicio {
 
     
     public static void main(String[] args) {
-        
+        System.out.println("pas");
     }
 
 }
