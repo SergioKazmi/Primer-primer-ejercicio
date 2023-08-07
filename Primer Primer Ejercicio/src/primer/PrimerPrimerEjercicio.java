ecd706f71084920a2d3d9c2a8f7c4aedd77eba4b
SergioKazmi <skazmiruk@outlook.com>
2023-08-07 17:22:13 -0300

    Cambios en el main
    Borre los errores que aparecian

--- Primer Primer Ejercicio/src/primer/PrimerPrimerEjercicio.java
+++ Primer Primer Ejercicio/src/primer/PrimerPrimerEjercicio.java
@@ -10,11 +10,9 @@ public class PrimerPrimerEjercicio {
 
     
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         System.out.println("pas");
-=======
 //        Hola que tal, soy Juan
->>>>>>> be0818ef583505d121b66d28b63acd9027127a6d
     }
 
 }
