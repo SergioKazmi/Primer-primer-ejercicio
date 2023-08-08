f7a73b833c286a654ee65050e35829a0f0a82118
eliasstein <eliasstein@users.noreply.github.com>
2023-08-08 15:33:09 -0300

    sumar 2 numeros
    Se modifico el codigo para que permita sumar 2 numeros

--- Primer Primer Ejercicio/src/primer/PrimerPrimerEjercicio.java
+++ Primer Primer Ejercicio/src/primer/PrimerPrimerEjercicio.java
@@ -1,17 +1,21 @@
-
-
 package primer;
-
-/**
- *
- * lee esto
- */
+import java.util.Scanner;
 public class PrimerPrimerEjercicio {
 
     
     public static void main(String[] args) {
-
-        System.out.println("pas");
+        int num1,num2;
+        Scanner scan=new Scanner(System.in);
+        try{
+        System.out.println("Inserte el primer numero");
+        num1=scan.nextInt();
+        System.out.println("Inserte el segundo numero");
+        num2=scan.nextInt();
+        System.out.println("El resultado es: "+(num1+num2));
+        }
+        catch(java.util.InputMismatchException ex){
+            System.out.println("ERROR el valor ingresado debe ser de tipo entero");
+        }
 //        Hola que tal, soy Juan
     }
 
